c980d71457a908b7c6cdc7af2dca35075717a488
aegorov <anat-gor@ya.ru>
2024-09-14 19:32:20 +0300

    sync service interface and implementation

--- src/Service/Contract/GigaChatServiceInterface.php
+++ src/Service/Contract/GigaChatServiceInterface.php
@@ -3,7 +3,13 @@ declare(strict_types=1);
 
 namespace Talismanfr\GigaChat\Service\Contract;
 
+use Talismanfr\GigaChat\Domain\Entity\Dialog;
+use Talismanfr\GigaChat\Domain\VO\Models;
+use Talismanfr\GigaChat\Service\Response\CompletionResponse;
+
 interface GigaChatServiceInterface
 {
+    public function models(): Models;
 
+    public function completions(Dialog $dialog): CompletionResponse;
 }
